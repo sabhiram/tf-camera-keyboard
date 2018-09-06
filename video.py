47a8bffe4fb46a17c0b00417ce86dac74faf8fe4
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2018-09-06 18:05:20 -0400

    Subtract bg

--- video.py
+++ video.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 import cv2
+import numpy as np
+import math
 
+bgModel = cv2.createBackgroundSubtractorMOG2()
 cap = cv2.VideoCapture(0)
+
 while True:
     ret, frame = cap.read()
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
 
-    cv2.imshow("frame", rgb)
+    mask = bgModel.apply(frame, learningRate=0)
+    kernel = np.ones((3, 3), np.uint8)
+    mask = cv2.erode(mask, kernel, iterations=1)
+    frame = cv2.bitwise_and(frame, frame, mask=mask)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (41, 41), 0)
+    ret, thresh = cv2.threshold(blur, 60, 255, cv2.THRESH_BINARY)
+
+    cv2.imshow("", thresh)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
