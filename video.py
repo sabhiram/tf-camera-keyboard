0007840a968998893f51ef9966272505162ff036
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2018-09-12 21:01:45 -0700

    Hand detection improvements

--- video.py
+++ video.py
@@ -5,24 +5,18 @@ import numpy as np
 import math
 import copy
 
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
 
-bg = None
+count     = 0
 show_orig = False
 
-while True:
-    ret, frame = cap.read()
-    if frame is None:
-        break
 
-    if bg is None:
-        bg = np.copy(frame)
+def method_1(frame, bg):
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    bg = cv2.cvtColor(bg, cv2.COLOR_BGR2GRAY)
 
     diff = cv2.absdiff(frame, bg)
-    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (41, 41), 0)
-    ret, thresh = cv2.threshold(blur, 30, 255, cv2.THRESH_BINARY)
+    blur = cv2.GaussianBlur(diff, (41, 41), 0)
+    ret, thresh = cv2.threshold(blur, 45, 200, cv2.THRESH_BINARY)
     t = copy.deepcopy(thresh)
     _, cont, hier = cv2.findContours(t, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -31,14 +25,56 @@ while True:
         hull = cv2.convexHull(res)
         cv2.drawContours(output, [res], 0, (0, 255, 0), 2)
         cv2.drawContours(output, [hull], 0, (0, 0, 255), 3)
+    return output
 
-    if show_orig:
-        cv2.imshow("", frame)
-    else:
-        cv2.imshow("", output)
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+def method_2(frame, bg):
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    bg    = cv2.cvtColor(bg, cv2.COLOR_BGR2GRAY)
+    frame = cv2.absdiff(bg, frame)
 
-cap.release()
-cv2.destroyAllWindows()
+    frame = cv2.medianBlur(frame, 5)
+    frame = cv2.GaussianBlur(frame, (5, 5), 0)
+    frame = cv2.bilateralFilter(frame, 15, 75, 75)
+    frame = cv2.GaussianBlur(frame, (7, 7), 0)
+    _, th = cv2.threshold(frame, 40, 255, cv2.THRESH_BINARY)
+
+    global count
+    count += 1
+    if count == 200:
+        print "Background settled!"
+    return th
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1000)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
+
+    bg, out = None, []
+    while True:
+        ret, frame = cap.read()
+        if frame is None:
+            break
+
+        if bg is None:
+            bg = np.copy(frame)
+            continue
+        else:
+            out = method_2(frame, bg)
+
+        if show_orig:
+            cv2.imshow("output", frame)
+        else:
+            cv2.imshow("output", out)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
